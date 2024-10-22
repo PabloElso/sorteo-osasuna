86dabfbcacc173849e428e1d2bb77eaea20f1395
PabloElso <pelso49@gmail.com>
2024-10-22 21:38:23 +0200

    Primeras definiciones

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Participante(models.Model):
+
+    millar = models.IntegerField()
+    posicion_millar = models.IntegerField()
+    numero_socio = models.IntegerField()
+    nombre_y_apellidos = models.CharField(max_length=100)
